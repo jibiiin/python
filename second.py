d46a08599bdd8a7d831c5652cb3653b774f90894
jibiiin <jibinkichu21@gmail.com>
2024-04-25 10:32:07 +0530

    its my second program

---
+++ second.py
@@ -0,0 +1,18 @@
+n=int(input("enter 1st number"))
+m=int(input("enter 2nd number"))
+def sum(n,m):
+    c=n+m
+    return c
+print(sum(n,m))
+def multiplication(n,m):
+    d=n*m
+    return d
+print(multiplication(n,m))  
+def subtraction(n,m):
+    e=n-m
+    return e
+print(subtraction(n,m))
+def division(n,m):
+    f=n//m
+    return f
+print(division(n,m))
