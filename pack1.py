fd9dc4cb03a6dadeda5c25577faca82e6ace2aac
jibiiin <jibinkichu21@gmail.com>
2024-04-25 12:03:13 +0530

    package of pythons1

---
+++ pack1.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    c=a+b
+    print("sum    :",c)
+def sub(a,b):
+    c=a-b
+    print("sub     :",c)
+def mul(a,b):
+    c=a*b
+    print("mul   :",c)
+def div(a,b):
+    c=a//b
+    print("div   :",c)        
---
+++ pack2.py
@@ -0,0 +1,5 @@
+from pack1 import *
+add(10,20)
+sub(20,10)
+mul(10,20)
+div(20,10)
