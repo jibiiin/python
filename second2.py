685990debf6e680a080167dde9b581983267ad87
jibiiin <jibinkichu21@gmail.com>
2024-05-03 10:28:09 +0530

    details

--- second2.py
+++ second2.py
@@ -1,4 +1,11 @@
-file=open("text.txt","w")
-file.write("hello world")
-print("good morning")
-file.close()
+file=open("text123.txt","a")
+def add():
+    name=input("enter your name")
+    address=input("enter your address")
+    phone=input("enter phone number")
+    file.write("name:"+name+"\naddress :"+address+"\nphone:"+phone+"\n_____________\n")
+    print(name+ "details added succesfully")
+for a in range(3):
+    add() 
+
+
