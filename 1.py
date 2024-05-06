e02a389577c3b5b2ce74093e88f205b4fa2c12e6
jibiiin <jibinkichu21@gmail.com>
2024-05-06 10:29:23 +0530

    selected succesfully

--- 1.py
+++ 1.py
@@ -1,20 +1,37 @@
-file=open("data.txt","w")
-select=int(input("select a number(1 Add, 2 Select, 3 Edit, 4 Delete)"))
 def add():
+    file=open("data.txt","a")
+    select=int(input("select a number(1 Add, 2 Select, 3 Edit, 4 Delete)"))
     if select==1:
         n=input("enter a name:")
         file.write(n+"\n")
         print(n+" Added successfully")
+        file.close()
         add()
     elif select==2:
-        file=open("data.txt","r")
-        content=file.read()
-        n=input("enter a selected id: ")
+        file1=open("data.txt","r")
+        content=file1.read()
+        n=input("enter a selected name: ")
         if n in content:
             print("Your Selected :"+n)
         else:
             print("Name not found")
         file.write(n+"\n")
-        print(n+" selected succesfully")
+        print(n+" selected succesfully")        
+        add()
     elif select==4:
-        file.open("data.txt","r")    
+        file1.open("data.txt","r")    
+        name=input("enter a name:")
+        content=file1.read()
+        if name in content:
+            li=list(content.split("\n"))
+            data=li.index(name)
+            del li[data]
+            str1="\n"    
+            newdata=str1.join(li)
+            file1=open("data.txt","w")
+            file1.write(newdata) 
+            print(name + "Deleted succesfully")
+            add()
+        else:
+            print("No")
+add()
