3eeaec164e885f9680e8e2f197e179bcd698246c
jibiiin <jibinkichu21@gmail.com>
2024-05-06 09:52:14 +0530

    added succesfully

--- 1.py
+++ 1.py
@@ -1,23 +1,20 @@
-name=["A","B","C"]
+file=open("data.txt","w")
 select=int(input("select a number(1 Add, 2 Select, 3 Edit, 4 Delete)"))
-if select==1:
-    n=input("enter a name:")
-    name.append(n)
-    print(name)
-elif select==2:
-    n=input("enter a selected name:")
-    data=name.index(n)
-    print("Your selected name :",name[data])
-elif select==3:
-    n=input("enter a name")
-    m=input("replaced name")
-    data=name.index(n)
-    name[data]=m
-    print(name)
-else:
-    n=input("enter a name")
-    data=name.index(n)
-    del name[data]
-    print(name)
-
-        
+def add():
+    if select==1:
+        n=input("enter a name:")
+        file.write(n+"\n")
+        print(n+" Added successfully")
+        add()
+    elif select==2:
+        file=open("data.txt","r")
+        content=file.read()
+        n=input("enter a selected id: ")
+        if n in content:
+            print("Your Selected :"+n)
+        else:
+            print("Name not found")
+        file.write(n+"\n")
+        print(n+" selected succesfully")
+    elif select==4:
+        file.open("data.txt","r")    
