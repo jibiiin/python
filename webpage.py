aaa7c28450ffddbd4a938f0846df70e5c0853b88
jibiiin <jibinkichu21@gmail.com>
2024-05-07 09:51:00 +0530

    webpage

---
+++ webpage.py
@@ -0,0 +1,18 @@
+file=open("index.html","w")
+title=input("enter your site title :")
+heading=input("enter your site heading:")
+para=input("enter your site content:")
+color=input("enter your bg color:")
+file.write("""  
+<html>
+           <head>
+             <title>%s</title>
+           </head>
+           <body bgcolor='%s'>
+              <h1>%s</h1>
+              <p>%s</p>
+           </body>
+</html>
+""" %(color,title,heading,para))
+print("webpage created successfully")                      
+           
