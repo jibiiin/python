d234a985c14c89660ff22f9b28d5ef3a241fb1eb
jibiiin <jibinkichu21@gmail.com>
2024-05-13 10:42:55 +0530

    tablefields

--- tabledatabase.py
+++ tabledatabase.py
@@ -1,8 +1,13 @@
 import mysql.connector
 conn=mysql.connector.connect(host="localhost",user="root",passwd="",database="mysql")
 cur=conn.cursor()
+name=input("enter table name")
+name1=input("enter fieldname1")
+name2=input("enter fieldname2")
+name3=input("enter fieldname3")
+
 if conn:
-    cur.execute("create table student(name char(20),eng int,math int)")
+    cur.execute("create table %s(%s char(20),%s int,%s int)"%(name,name1,name2,name3))
     print("Table created success")
 else:
     print("connection failed")    
