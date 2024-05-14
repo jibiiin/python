e9f345972edcb950d48c12a028cb414fa27f16be
jibiiin <jibinkichu21@gmail.com>
2024-05-14 11:22:05 +0530

    selectfromtable

--- selectfrmtbl.py
+++ selectfrmtbl.py
@@ -1,10 +1,13 @@
 import mysql.connector
 conn=mysql.connector.connect(host="localhost",user="root",passwd="",database="mysql")
 cur=conn.cursor()
+name=input("Enter a name :")
 if conn:
-    cur.execute("select *from jiji")
+    cur.execute("select *from jiji where name='%s'"%name)
     result=cur.fetchall()
     for a in result:
-        print("Name :",a[0],a[1])
+        print("name :",a[0])
+        print("mark1 :",a[1])
+        print("mark2 :",a[2])
 else:
     print("connection failed")    
--- tabledatabase.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-conn=mysql.connector.connect(host="localhost",user="root",passwd="",database="mysql")
-cur=conn.cursor()
-if conn:
-    cur.execute("insert into jiji value('anu',20,30)")
-    conn.commit()
-    print("Insert data created success")
-else:
-    print("connection failed")    
