0819a9a8350d65804792f806a50aaa312a32532b
jibiiin <jibinkichu21@gmail.com>
2024-05-18 17:54:51 +0530

    packk

---
+++ packk.py
@@ -0,0 +1,26 @@
+from tkinter import *
+def click(num):
+    first_value=str(e1.get())
+    e1.delete(0,END)
+    e1.insert(0,first_value+str(num))
+
+window=Tk()
+window.title("welcome")
+window.geometry("220x300")
+window.resizable(False,False)
+window.configure(background="#50252d")
+window.attributes("-topmost",True)
+window.attributes("-alpha",0.9)
+window.iconbitmap("favicon.ico")
+
+e1=Entry()
+one=Button(text="1",command=lambda:click(1))   
+two=Button(text="2",command=lambda:click(2))
+add=Button(text="+")
+equal=Button(text="=")
+e1.pack()
+one.pack()
+two.pack()
+add.pack()
+equal.pack()
+window.mainloop()             
